e869ef078b21dfbedaae15c1128e83c3fd686535
JoshNicked <151979241+JoshNicked@users.noreply.github.com>
2026-03-27 11:20:23 +0800

    Several UI changes

--- Assets/Scripts/Landing_Script/ToLevels.cs
+++ Assets/Scripts/Landing_Script/ToLevels.cs
@@ -6,6 +6,7 @@ public class ToLevels : MonoBehaviour
 
     public void LoadSceneLanding(int sceneIndex)
     {
+        Time.timeScale = 1f; //Researched for an hour only to find out I just need 1 line of code to load animations, PAIN
         SceneManager.LoadScene(0);
     }
     
